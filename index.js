1e2116496fd7e754da5d9071789ff936d5ec3b27
danap3959 <69054833+danap3959@users.noreply.github.com>
2023-10-13 22:43:16 -0400

    updates to pug and index file, new routes created;)

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = 'pat-na1-6cae2239-b1e8-44a7-8484-14ef62c39dfe';
+const PRIVATE_APP_ACCESS = "pat-na1-6cae2239-b1e8-44a7-8484-14ef62c39dfe";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts?properties=email,favorite_color,nick_name,firstname,lastname,hair_length';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -22,7 +22,24 @@ app.get('/contacts', async (req, res) => {
     try {
         const resp = await axios.get(contacts, { headers });
         const data = resp.data.results;
-        res.render('homepage', { title: 'Add/Update this table', data });      
+        //res.json(data);
+        res.render('homepage', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get('/contacts', async (req, res) => {
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts?properties=email,favorite_color,nick_name,firstname,lastname,hair_length';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        //res.json(data);
+        res.render('contacts', { title: 'Test', data });      
     } catch (error) {
         console.error(error);
     }
@@ -32,9 +49,10 @@ app.get('/contacts', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 app.get('/update', async (req, res) => {
     // http://localhost:3000/update?email=jessiewaters@gmail.com
-    const email = req.query.email;
+    const encodedEmail = req.query.email;
+    const email = decodeURIComponent(encodedEmail);
 
-    const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,favorite_color,nick_name`;
+    const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,favorite_color,nick_name,hair_length`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -45,7 +63,7 @@ app.get('/update', async (req, res) => {
         const data = response.data;
 
         //res.json(data);
-        res.render('update', {userEmail: data.properties.email, favoriteColor: data.properties.favorite_color, nickName: data.properties.nick_name});
+        res.render('update', {userEmail: data.properties.email, favoriteColor: data.properties.favorite_color, nickName: data.properties.nick_name, hairLength: data.properties.hair_length});
         
     } catch(err) {
         console.error(err);
@@ -60,8 +78,8 @@ app.post('/update', async (req, res) => {
     const update = {
         properties: {
             "nick_name": req.body.newVal2,
-            "favorite_color": req.body.newVal
-            
+            "favorite_color": req.body.newVal,
+            "hair_length": req.body.newVal3
         }
     }
 
@@ -75,6 +93,7 @@ app.post('/update', async (req, res) => {
     try { 
         await axios.patch(updateContact, update, { headers } );
         res.redirect('back');
+      
     } catch(err) {
         console.error(err);
     }
